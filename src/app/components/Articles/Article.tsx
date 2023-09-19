3f266e11443ee772f44e9c155e28f0db13dc0df4
santiago dibene <dibenesantiago@gmail.com>
2023-09-18 23:21:21 -0300

    feat: render article data from api

--- src/app/components/Articles/Article.tsx
+++ src/app/components/Articles/Article.tsx
@@ -1,13 +1,19 @@
+import { ArticleRaw } from "@/app/api/request/articles/get";
 import React from "react";
 
-export function Article() {
+interface ArticleProps {
+  article: ArticleRaw;
+}
+export function Article({ article }: ArticleProps) {
   return (
     <article className="mod-caja-nota lugares w-100-mobile">
       <section id="" className="cont-figure">
         <a href="" className="figure">
           <picture id="" className="content-pic picture">
+            {/* adding provisory style because the resizedUrls doesn't work */}
             <img
-              src="https://bucket1.glanacion.com/anexos/fotos/22/3054322h600.jpg"
+              style={{ height: "100%", width: "100%" }}
+              src={article.promo_items.basic.url}
               alt=""
               className="content-img"
             />
@@ -16,13 +22,38 @@ export function Article() {
       </section>
       <div className="mod-caja-nota__descrip">
         <h2 className="com-title-acu">
-          <a href="">
-            <b>La escuela.</b> que tiene de escudo al Che Guevara y donde izan
-            la bandera de Cuba
-          </a>
+          <a href={article.website_url}>{article.headlines.basic}</a>
         </h2>
-        <h4 className="com-date">1 de Julio de 2019</h4>
+        <h4 className="com-date">{formatDate(article.display_date)}</h4>
       </div>
     </article>
   );
+}
+
+/**
+ * receive the date in this format 2019-06-01T15:14:40.626Z
+ * and return in this format 1 de Julio de 2019
+ */
+function formatDate(date: string) {
+  const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+
+  const fecha = new Date(date);
+  const day = fecha.getDate();
+  const month = fecha.getMonth();
+  const year = fecha.getFullYear();
+
+  return `${day} de ${months[month]} de ${year}`;
 }
--- src/app/components/Articles/Articles.tsx
+++ src/app/components/Articles/Articles.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { Article } from "./Article";
 import { LoadMoreButton } from "./LoadMoreButton";
 import { MobileBanner } from "./MobileBanner";
+import { get } from "@/app/api/request/articles/get";
 
-export function Articles() {
+export async function Articles() {
+  const data = await get();
+
+  const filteredArticles = data.articles.filter(
+    (article) => article.subtype === "7"
+  );
+
   return (
     <>
       <section className="row-gap-tablet-2 row-gap-deskxl-3 hlp-degrade">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
+        {filteredArticles.map((article, index) => (
           <>
-            <Article key={index} />
-            {((index % 3 === 2)) && <MobileBanner />}
+            <Article key={article._id} article={article} />
+            {index % 3 === 2 && <MobileBanner />}
           </>
         ))}
       </section>
